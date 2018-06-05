c3915bab334dd2b257597311d9bca64292e15b88
Cristian Dorneanu <cristian.dorneanu@fortech.ro>
2018-06-05 14:03:02 +0300

    initial commit

---
+++ collabtext-server/src/main/java/com/freelancer/developer/collabtext/CollabtextApplication.java
@@ -0,0 +1,12 @@
+package com.freelancer.developer.collabtext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabtextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabtextApplication.class, args);
+	}
+}
